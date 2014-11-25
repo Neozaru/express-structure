e345f7c77ee9ce9c79dd2b3d949fc29b7b093c98
Neozaru <neozaru@mailoo.org>
2014-11-25 23:53:00 +0100

    Trying generic functions

--- controllers/cats.js
+++ controllers/cats.js
@@ -1,72 +1,26 @@
+var generic_rest = require('./generic_rest');
 /* Let caller define which model use (better architecture, esier testing) */
 var catsInj = function(CatModel) {
 
 	var cats = {};
 	cats.index = function(req, res) {
-		return CatModel.find(function(err, cats) {
-			return err ? res.sendStatus(500) : res.send(cats);
-		});
+		return generic_rest.index(CatModel, req, res);
 	}
 
 	cats.create = function(req, res) {
-		if (!req.body.name) {
-			return res.status(400).send({error: "Missing 'name'"});
-		}
-		
-		var cat = new CatModel({
-			name: req.body.name
-		});
-
-		cat.save(function(err) {
-			return err ? res.sendStatus(500) : res.status(201).send(cat);
-		});
+		return generic_rest.create(CatModel, ["name", "mewowed"], ["name"], req, res);
 	}
 
 	cats.get = function(req, res) {
-		return CatModel.findById(req.params.id, function(err, cat) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-
-			return cat ? res.send(cat) : res.sendStatus(404);
-			
-		});
+		return generic_rest.get(CatModel, req.params.id, req, res);
 	}
 
 	cats.delete = function(req, res) {
-		return CatModel.findById(req.params.id, function(err, cat) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-	
-			return cat.remove(function(err) {
-				return  res.sendStatus(err ? 500 : 202)
-			});
-	
-		});
+		return generic_rest.delete(CatModel, req.params.id, req, res);
 	}
 
 	cats.edit = function(req, res) {
-		return CatModel.findById(req.params.id, function(err, cat) {
-			if (err) {
-				console.log(err);
-				return res.sendStatus(500);
-			}
-
-			if (req.body.name) {
-				cat.name = req.body.name;
-			}
-			if (req.body.mewowed) {
-				cat.mewowed = req.body.mewowed;
-			}
-	
-			return cat.save(function(err) {
-				return  err ? res.sendStatus(500) : res.send(cat);
-			});
-	
-		});
+		return generic_rest.edit(CatModel, req.params.id, ["name", "mewowed"], req, res);
 	}
 
 	return cats;
---
+++ controllers/generic_rest.js
@@ -0,0 +1,80 @@
+var _ = require('underscore');
+
+var generic = {};
+
+function copyFields(source, target, fields, required) {
+    return _.reduce(fields, function(field) {
+        if (!_.isUndefined(source[field])) {
+            target[field] = source[field];
+        }
+        else if (_.contains(required,field)) {
+            console.log(field);
+            return field;
+        }
+    });
+}
+
+generic.index = function(Model, req, res) {
+    return Model.find(function(err, items) {
+        return err ? res.sendStatus(500) : res.send(items);
+    });
+}
+
+generic.create = function(Model, fields, required, req, res) {
+    
+    var item = new Model();
+    var missing = copyFields(req.body, item, fields, required);
+    if (!_.isEmpty(missing)) {
+        return res.status(400).send({"error": {"missing": missing}});
+    }
+
+    item.save(function(err) {
+        return err ? res.sendStatus(500) : res.status(201).send(item);
+    });
+}
+
+generic.get = function(Model, id, req, res) {
+    return Model.findById(id, function(err, item) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        return item ? res.send(item) : res.sendStatus(404);
+        
+    });
+}
+
+generic.delete = function(Model, id, req, res) {
+    return Model.findById(req.params.id, function(err, item) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        return item.remove(function(err) {
+            return  res.sendStatus(err ? 500 : 202)
+        });
+
+    });
+}
+
+generic.edit = function(Model, id, fields, req, res) {
+    return Model.findById(id, function(err, item) {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        copyFields(req.body, item, fields, []);
+
+        return item.save(function(err) {
+            return  err ? res.sendStatus(500) : res.send(item);
+        });
+
+    });
+}
+
+
+
+module.exports = generic;
