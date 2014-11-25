448da39d541a2077dc5dc57d5acfd248deec3ea7
Neozaru <neozaru@mailoo.org>
2014-11-25 23:53:26 +0100

    Tab to spaces

--- controllers/cats.js
+++ controllers/cats.js
@@ -2,29 +2,29 @@ var generic_rest = require('./generic_rest');
 /* Let caller define which model use (better architecture, esier testing) */
 var catsInj = function(CatModel) {
 
-	var cats = {};
-	cats.index = function(req, res) {
-		return generic_rest.index(CatModel, req, res);
-	}
+    var cats = {};
+    cats.index = function(req, res) {
+        return generic_rest.index(CatModel, req, res);
+    }
 
-	cats.create = function(req, res) {
-		return generic_rest.create(CatModel, ["name", "mewowed"], ["name"], req, res);
-	}
+    cats.create = function(req, res) {
+        return generic_rest.create(CatModel, ["name", "mewowed"], ["name"], req, res);
+    }
 
-	cats.get = function(req, res) {
-		return generic_rest.get(CatModel, req.params.id, req, res);
-	}
+    cats.get = function(req, res) {
+        return generic_rest.get(CatModel, req.params.id, req, res);
+    }
 
-	cats.delete = function(req, res) {
-		return generic_rest.delete(CatModel, req.params.id, req, res);
-	}
+    cats.delete = function(req, res) {
+        return generic_rest.delete(CatModel, req.params.id, req, res);
+    }
 
-	cats.edit = function(req, res) {
-		return generic_rest.edit(CatModel, req.params.id, ["name", "mewowed"], req, res);
-	}
+    cats.edit = function(req, res) {
+        return generic_rest.edit(CatModel, req.params.id, ["name", "mewowed"], req, res);
+    }
 
-	return cats;
-	
+    return cats;
+    
 }
 
 
