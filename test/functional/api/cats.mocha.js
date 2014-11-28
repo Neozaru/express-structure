48d18191cecb0f54606b83b70e8199f932655537
Neozaru <neozaru@mailoo.org>
2014-11-28 21:56:48 +0100

    More functional tests

--- test/functional/api/cats.mocha.js
+++ test/functional/api/cats.mocha.js
@@ -1,4 +1,4 @@
-var sequence = require('futures').sequence();
+var futures = require('futures');
 var request = require('supertest');
 var mongoose = require('mongoose');
 var expect = require('chai').expect;
@@ -14,46 +14,131 @@ describe('Generic Rest Model test', function() {
   });
 
   describe('Cats API', function() {
-    it('should get existing cats', function(done) {
 
-        /* I saw the future */
-        sequence.then(function(next) {
-          request(app)
-            .get('/api/cats')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .expect({})
-            .end(next);
-        })
+    it('should create and get existing cats', function(done) {
+      /* I saw the future */
+      futures.sequence().then(function(next) {
+        request(app)
+          .get('/api/cats')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .expect({})
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .post('/api/cats')
+          .send({"name": "mycat1"})
+          .expect(200)
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .post('/api/cats')
+          .send({"name": "mycat2", "mewowed": true})
+          .expect(200)
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .get('/api/cats')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .expect(function(res) {
+            expect(res.body).to.have.length(2);
+            expect(res.body[0]).to.have.property("name", "mycat1");
+            expect(res.body[0]).to.have.property("mewowed", false);
+            expect(res.body[1]).to.have.property("name", "mycat2");
+            expect(res.body[1]).to.have.property("mewowed", true);
+          })
+          .end(done);
+      });
+    });
 
-        .then(function(next) {
-          request(app)
-            .post('/api/cats')
-            .send({"name": "mycat1"})
-            .expect(200)
-            .end(next);
-        })
-        .then(function(next) {
-          request(app)
-            .post('/api/cats')
-            .send({"name": "mycat2", "mewowed": true})
-            .expect(200)
-            .end(next);
-        })
-        .then(function(next) {
-          request(app)
-            .get('/api/cats')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .expect(function(res) {
-              expect(res.body).to.have.length(2);
-              expect(res.body[0]).to.have.property("name", "mycat1");
-              expect(res.body[0]).to.have.property("mewowed", false);
-              expect(res.body[1]).to.have.property("name", "mycat2");
-              expect(res.body[1]).to.have.property("mewowed", true);
-            })
-            .end(done)
-        });
+    it('should get cat by id', function(done) {
+      var catid;
+      /* I saw the future */
+      futures.sequence().then(function(next) {
+        request(app)
+          .post('/api/cats')
+          .send({"name": "mycat1"})
+          .expect(201)
+          .expect(function(res) {
+            catid = res.body._id;
+          })
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .get('/api/cats/'+catid)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .expect(function(res) {
+            expect(res.body).to.have.property("name", "mycat1");
+            expect(res.body).to.have.property("mewowed", false);
+          })
+          .end(done);
+      });
+    });
+
+    it('should delete cat properly', function(done) {
+      var catid;
+      /* I saw the future */
+      futures.sequence().then(function(next) {
+        request(app)
+          .post('/api/cats')
+          .send({"name": "mycat1"})
+          .expect(201)
+          .expect(function(res) {
+            catid = res.body._id;
+          })
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .delete('/api/cats/'+catid)
+          .expect(202)
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .get('/api/cats/'+catid)
+          .expect(404)
+          .end(done);
+      });
+    });
+
+    it('should change cat', function(done) {
+      var catid;
+      /* I saw the future */
+      futures.sequence().then(function(next) {
+        request(app)
+          .post('/api/cats')
+          .send({"name": "mycat1"})
+          .expect(201)
+          .expect(function(res) {
+            catid = res.body._id;
+          })
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .put('/api/cats/'+catid)
+          .send({"name": "mycatchanged", "mewowed": true})
+          .expect(200)
+          .end(next);
+      })
+      .then(function(next) {
+        request(app)
+          .get('/api/cats/'+catid)
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .expect(function(res) {
+            expect(res.body).to.have.property("name", "mycatchanged");
+            expect(res.body).to.have.property("mewowed", true);
+          })
+          .end(done);
+      });
     });
 
   });
